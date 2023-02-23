ce4d7ab32b8308b97aeda1a82700f13e7fcee7bb
fureszb <fureszb@gmail.com>
2023-02-23 16:45:38 +0100

    eq

--- Equalizer.java
+++ Equalizer.java
@@ -1,29 +1,46 @@
 
 package feldat;
 
-public class Equalizer {
-     public static void main(String[] args) {
-         for (int i = 0; i < 3; i++) {
-             System.out.print("\u001B[45m" + " ");
-         }
-         System.out.println("");
-         for (int i = 0; i < 8; i++) {
-             System.out.print("\u001B[45m" + " ");
-         }
-         System.out.println("");
-         for (int i = 0; i < 4; i++) {
-             System.out.print("\u001B[45m" + " ");
-         }
-         System.out.println("");
-         for (int i = 0; i < 7; i++) {
-             System.out.print("\u001B[45m" + " ");
-         }
-         System.out.println("");
-         for (int i = 0; i < 8; i++) {
-             System.out.print("\u001B[45m" + " ");
-         }
-         
+import java.util.Random;
 
+public class Equalizer {
+    private static Random rnd = new Random();
+     public static void main(String[] args) {
+         eq();
+         eq(12);
+         eq(8, true);
+         eq(true);
 
     }
+
+    private static void eq(){
+        int db = rnd.nextInt(3,7);
+        eq(db);
+    }
+    private static void eq(int hossz){
+        int db = hossz; 
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.print(szin+" ");
+        }
+        System.out.println("");
+        }
+    private static void eq(int hossz, boolean kiiras){
+        int db = hossz; 
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.print(szin+" ");
+        }
+        if (kiiras == true) {
+            System.out.printf("(%d)",db);
+        }
+        System.out.println("");
+        }
+     private static void eq(boolean kiiras){
+        int db = rnd.nextInt(3,7);
+        eq(db, kiiras);
+    }
+    
+    
+
 }
