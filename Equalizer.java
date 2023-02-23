131168b15e842659b51ce67fa2a9e37ac187f4f2
fureszb <fureszb@gmail.com>
2023-02-23 16:17:39 +0100

    eqKiir

--- Equalizer.java
+++ Equalizer.java
@@ -3,6 +3,27 @@ package feldat;
 
 public class Equalizer {
      public static void main(String[] args) {
-        
+         for (int i = 0; i < 3; i++) {
+             System.out.print("\u001B[45m" + " ");
+         }
+         System.out.println("");
+         for (int i = 0; i < 8; i++) {
+             System.out.print("\u001B[45m" + " ");
+         }
+         System.out.println("");
+         for (int i = 0; i < 4; i++) {
+             System.out.print("\u001B[45m" + " ");
+         }
+         System.out.println("");
+         for (int i = 0; i < 7; i++) {
+             System.out.print("\u001B[45m" + " ");
+         }
+         System.out.println("");
+         for (int i = 0; i < 8; i++) {
+             System.out.print("\u001B[45m" + " ");
+         }
+         
+
+
     }
 }
